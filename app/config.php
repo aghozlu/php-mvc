2413f9847369d374c993402eeba3e38d653f34eb
Ali Aghozlu <34648226+aghozlu@users.noreply.github.com>
2020-01-31 00:16:59 +0330

    Create config.php

---
+++ app/config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aghozlu-pc
+ * Date: 1/4/2020
+ * Time: 5:35 PM
+ */
+
+session_start();
+
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'mvc_php');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_CHARSET', 'utf8');
+define('URL', 'http://localhost/mvc/');
+
+define('PASSMD5', '_aZ87JkgfB076MDsybqpz521BQ52hWpcEcgA458');
+define('USERMD5', '-fGceYjKL234XZi09Pu6Q1aFwH56D78BnsMD5fJ');
