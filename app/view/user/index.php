e516d8463e100401b66200dd938733cbeddb239b
Ali Aghozlu <34648226+aghozlu@users.noreply.github.com>
2020-01-31 00:30:14 +0330

    Create index.php

---
+++ app/view/user/index.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+    <title>
+        signup
+    </title>
+
+</head>
+<body>
+<h1>Sign Up</h1>
+<form action="signup" method="post">
+
+    <input type="email"  name="email" placeholder="email"/>
+    <input type="password"  name="password" placeholder="password"/>
+
+    <button>Sign Up</button>
+</form>
+<hr />
+<h1>Sign In</h1>
+<form action="signin" method="post">
+
+    <input type="text"  name="email" placeholder="email or username"/>
+    <input type="password"  name="password" placeholder="password"/>
+
+    <button>Sign in</button>
+</form>
+</body>
+</html>
+
