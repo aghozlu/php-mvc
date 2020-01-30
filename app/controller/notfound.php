28b6d1c8e5176aac0315b9b186c4cea83d4e1142
Ali Aghozlu <34648226+aghozlu@users.noreply.github.com>
2020-01-31 00:23:10 +0330

    Create notfound.php

---
+++ app/controller/notfound.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aghozlu-pc
+ * Date: 1/4/2020
+ * Time: 5:37 PM
+ */
+
+class Notfound extends Controller
+{
+
+    public function index(){
+       $this->view("notfound/index");
+    }
+}
