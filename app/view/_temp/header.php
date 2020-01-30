8548a1ada3045209ad6148c0d11b5ccf048c4924
Ali Aghozlu <34648226+aghozlu@users.noreply.github.com>
2020-01-31 00:27:51 +0330

    Create header.php

---
+++ app/view/_temp/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="./public/css/materialize.min.css" rel="stylesheet">
+    <link href="./public/css/stylecss.css" rel="stylesheet">
+
+    <script src="./public/jscript/jQuery.js"></script>
+    <script src="./public/jscript/materialize.min.js"></script>
+    <script src="./public/jscript/jscript.js"></script>
+
+</head>
+<body>
+<nav>
+    <div class="nav-wrapper blue">
+        <a href="<?=URL?>" class="brand-logo right">
+            <img src="public/image/logo.png" />
+        </a>
+        <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <li>
+                <a href="user">
+                    <i class="material-icons">account_circle</i>
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="row"></div>
